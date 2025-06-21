116aacc0b65a8ca147aeacb17d39628c5d750b4a
Alamin-Sani <abba.danadala@gmail.com>
2025-06-21 17:59:18 +0100

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -39,12 +39,12 @@ const Navbar = () => {
             >
               Roadmap
             </a>
-            {/* <a
-              href="#"
+            <a
+              href="#tokenomics"
               className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-md font-bold"
             >
-              Contact
-            </a> */}
+              Tokenomics
+            </a>
           </div>
 
           {/* Mobile Menu Button */}
